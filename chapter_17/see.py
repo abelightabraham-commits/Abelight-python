56525456cd52108be8ef622e1b44dba782d990f1
abelight <abelightabraham@gmail.com>
2026-09-17 04:37:20 +0100

    python project chapter 17 API request project

---
+++ chapter_17/see.py
@@ -0,0 +1,40 @@
+import requests
+from operator import itemgetter
+
+# Make an API call and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f'status code: {r.status_code}')
+
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Make a seperate API call for each submission.
+    url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
+    r = requests.get(url)
+    print(f'id: {submission_id}\tstatus: {r.status_code}')
+    response_dict = r.json()
+
+    # Skip items that dont have descendants.
+    if 'descendants' not in response_dict:
+        continue
+
+    # Build a dictionary for each article.
+    print(f'ID: {submission_id}')
+    print(f'Type: {response_dict.get('type')}')
+    print(f'Has descendants: {'descendants' in response_dict}')
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f'https://news.ycombinator.com/item?id={submission_id}',
+        'comments': response_dict['descendants'],
+
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\n'Title: {submission_dict['title']}")
+    print(f'Discussion link: {submission_dict['hn_link']}')
+    print(f'Comments: {submission_dict['comments']}')
